ca1267b694d17c121147095ef22dfcb42927d7f9
viktorfa <vikfand@gmail.com>
2018-03-04 14:09:09 +0100

    Make footer a little better.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import {Segment, Container} from 'semantic-ui-react';
 
 function Footer() {
-  return (
-    <footer style={{height: '100px', backgroundColor: 'rgb(80, 80, 80)'}}>
-      <div>
-        <div style={{color: 'whitesmoke'}}>© inneklemtedager.no</div>
-        <ul>
-        </ul>
-      </div>
-    </footer>
-  );
+    return (
+        <footer style={{backgroundColor: 'rgb(80, 80, 80)'}}>
+            <Segment vertical style={{padding: '2em 0'}}>
+                <Container>
+                    <div>
+                        <div style={{color: 'whitesmoke', fontSize: '1.4em'}}>© inneklemtedager.no</div>
+                        <br/>
+                        <div><a href="https://github.com/viktorfa/squeezed-days">GitHub</a></div>
+                        <div><a href="https://www.facebook.com/inneklemtedager/">Facebook</a></div>
+
+                    </div>
+                </Container>
+            </Segment>
+        </footer>
+    );
 }
 
 export default Footer;
